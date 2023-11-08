2cebdcc73850687ccdd0a6b148c25bd7ceb074ce
tako1995 <141060940+tako1995@users.noreply.github.com>
2023-11-08 05:56:07 +0400

    script

--- script.js
+++ script.js
@@ -1,2 +1,82 @@
-"use strict";
+// "use strict";
+
+// EX1
+
+function sum(...numbers) {
+  let numberSum = 0;
+  for (let item of numbers) {
+    numberSum += item;
+  }
+
+  return numberSum;
+}
+
+let sumResult = sum(10, 50, 6, 7, 8, 11, 6, 3, 9);
+console.log(sumResult);
+
+// EX2
+
+let user = {
+  firstname: "giorgi",
+  lastname: "saakadze",
+  age: 32,
+  isloggedin: true,
+};
+
+function printName(user) {
+  if (user.isloggedin) {
+    return `${user.firstname} ${user.lastname}`;
+  }
+  return false;
+}
+
+let printNameResult = printName(user);
+console.log(printNameResult);
+
+// EX3
+
+function getMax(numbers) {
+  let maxValue = 0;
+  for (let item of numbers) {
+    if (item > maxValue) {
+      maxValue = item;
+    }
+  }
+  return maxValue;
+}
+
+let getMaxvaleResult = getMax([14, 98, 55, 36, 97]);
+console.log(getMaxvaleResult);
+
+// EX4
+
+function testfnc(x) {
+  if (x % 2 == 0) {
+    return "this number is even";
+  }
+  return "this number is odd";
+}
+
+let testFncResult = testfnc(12);
+console.log(testFncResult);
+
+// EX5
+
+let array = [1, 2, 3, 4, 5];
+
+for (let i = array.length - 1; i >= 0; i--) {
+  console.log(array[i]);
+}
+
+// EX6
+
+
+let userAgeBirth = (birthYear, yearNow) => {
+    let userAge = yearNow - birthYear;
+    let resultAgebirth = userAge > 18 ? "სრულწლოვანი" : "არასრულწლოვანი";
+    return resultAgebirth;
+  };
+  
+  let lastResult = userAgeBirth(2000, 2023);
+  console.log(lastResult);
 
